750154b9ace5f623760cfcf7664d288c7968ff27
MariamKhalil2004 <merokhalell09@gmail.com>
2026-04-18 20:07:09 +0200

    Revert "RrafiQ"
    This reverts commit 06a8e9263662b032d12f4b23cc7f6b7731c5a82a.

--- lib/views/dashboard/widgets/planprogresscard/view.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-class PlanProgressCard extends StatelessWidget {
-  const PlanProgressCard ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Column();
-  }
-}
